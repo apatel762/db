25a7f6f8827e306092b65f84a0a9f932f2c4cc3d
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2024-01-08 21:20:25 +0000

    chore: disable commit msg line length check

---
+++ .github/workflows/java-ci-commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rules: {
+    // https://commitlint.js.org/#/reference-rules
+    // first param = 0 which means disable the rule
+    'body-max-line-length': [0, 'always', 100]
+  }
+};
